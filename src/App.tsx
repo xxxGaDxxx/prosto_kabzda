7102753f10232831c7916f1ce63ef3707a3ba949
Wlad loban <qwegadqwe@gmail.com>
2022-05-23 21:44:27 +0300

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Accordion} from './components/Accordion/Accordion.tssx';
+import {Rating} from './components/Rating/Rating';
+import {OnOff} from './components/OnOff/OnOff';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <PageTitle title={'This is APP'}/>
+            <Rating value={3}/>
+            <Accordion titleValue={'Menu'} colLapsed={true}/>
+            <Accordion titleValue={'WOW'} colLapsed={false}/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+            <OnOff on={true}/>
+            <OnOff on={false}/>
+            <Rating value={5}/>
+        </div>
+    );
 }
+
+type PagePropsType = {
+    title: string
+}
+
+function PageTitle(props: PagePropsType) {
+    return <h1>{props.title}</h1>
+}
+
 
 export default App;
